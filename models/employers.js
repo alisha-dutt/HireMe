c411a88f76e98a08fdde5eb55ce24b1d5930baac
alisha-dutt <200490302@student.georgianc.on.ca>
2023-02-15 16:02:26 -0500

    model added

--- models/employers.js
+++ models/employers.js
@@ -1,25 +1,24 @@
 const mongoose = require('mongoose');
 
-//create scehma for an employer document
+// create schema for an employer document
 const employerSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required
+    name: {
+        type: String,
+        required: 'Name is required'
     },
-    city:{
-        type:String,
-        required
+    city: {
+        type: String,
+        required: 'City is required'
     },
-    contactEmail:{
-        type:String
+    contactEmail: {
+        type: String
     },
-    comments:{
-        type:String
+    comments: {
+        type: String
     },
-    employees:{
-        type:Number
+    employees: {
+        type: Number
     }
 });
 
-//making the model public
-module.exports =mongoose.model('Employer',employerSchema);
+module.exports = mongoose.model('Employer', employerSchema);
